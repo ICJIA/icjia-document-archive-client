6aeab638093b4d5bba6ba41575b8fdbeebb25d8f
cschweda <cschweda@gmail.com>
2019-08-29 07:33:43 -0500

    fix: footer fix

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -13,7 +13,7 @@
     >
       <v-card-text>
         <v-btn
-          to="/files/adult-redeploy"
+          :href="`${$store.getters.config.archiveURL}/files/adult-redeploy`"
           depressed
           class="heavy "
           dark
@@ -24,7 +24,7 @@
         </v-btn>
 
         <v-btn
-          to="/files/icjia"
+          :href="`${$store.getters.config.archiveURL}/files/icjia`"
           depressed
           class="heavy "
           dark
@@ -34,7 +34,7 @@
           <span style="font-size: 12px">ICJIA archive</span>
         </v-btn>
         <v-btn
-          to="/files/spac"
+          :href="`${$store.getters.config.archiveURL}/files/spac`"
           depressed
           class="heavy "
           dark
