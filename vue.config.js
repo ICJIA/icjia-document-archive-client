ad3b9efcc87b2642869de2a772859be8e62aa741
cschweda <cschweda@gmail.com>
2019-09-25 08:48:15 -0500

    fix: config typo

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
-const config = require('@/config.json')
+const config = require('./src/config.json')
 
 module.exports = {
   publicPath: process.env.NODE_ENV === `production` ? config.publicPath : '/',
