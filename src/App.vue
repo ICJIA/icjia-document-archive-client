0e7d2970ef08410e77c8889f5d16d86eae4ee2fb
cschweda <cschweda@gmail.com>
2020-03-02 14:47:21 -0600

    fix: add warning

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <app-nav />
     <search-drawer />
     <v-content>
+      <corona ref="alert" />
       <router-view />
     </v-content>
     <app-footer />
@@ -13,17 +14,25 @@
   import AppNav from '@/components/AppNav'
   import AppFooter from '@/components/AppFooter'
   import SearchDrawer from '@/components/SearchDrawer'
+  import Corona from '@/components/Corona'
   export default {
     name: 'App',
     components: {
       AppNav,
       SearchDrawer,
       AppFooter,
+      Corona,
 
     },
     data: () => ({
     //
     }),
+    watch: {
+      // eslint-disable-next-line no-unused-vars
+      $route (to, from) {
+        if (this.$refs.alert) this.$refs.alert.reset()
+      },
+    },
     async created () {
       if (!this.$store.state.isAppReady) {
         this.$store.dispatch('init')
